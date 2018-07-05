12292cbf862186d36a35d76ace13b7f284f7bc55
Dejan <dejandavidovic1989@gmail.com>
2018-07-05 14:55:09 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Route} from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Contact from './Components/Contact';
